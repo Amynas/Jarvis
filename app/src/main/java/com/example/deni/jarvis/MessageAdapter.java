865797a316d0356166c86cc72f0f055fa71e73ff
Amynas <deniel.hruby@gmail.com>
2016-10-13 20:06:41 +0200

    fejlesztünk valahogy... :(

---
+++ app/src/main/java/com/example/deni/jarvis/MessageAdapter.java
@@ -0,0 +1,12 @@
+package com.example.deni.jarvis;
+
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+
+/**
+ * Created by Amynas on 2016.10.13..
+ */
+
+public class MessageAdapter  extends ArrayAdapter<String>{
+
+}
